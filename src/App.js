de9c8673b5b2c52eced07166fb03e9911971d654
tamilselvan182425 <tamilselvanppy18@gmail.com>
2024-01-18 11:53:50 +0530

    install reactjs and tailwind css and create the first template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2 className='text-2xl bg-blue-400'>tamilselvan</h2>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
