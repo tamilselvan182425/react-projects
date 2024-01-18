e89aef6b4b17d948b9497fd4dc2501d3ce156ffb
tamilselvan182425 <tamilselvanppy18@gmail.com>
2024-01-18 15:55:12 +0530

    craete pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./page/Home";
+import Profile from "./page/Profile";
+import SignIn from "./page/SignIn";
+import SignUp from "./page/SignUp";
+import ForgotPassword from "./page/ForgotPassword";
+import Offers from "./page/Offers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h2 className='text-2xl bg-blue-400'>tamilselvan</h2>
-       
-      </header>
-    </div>
+   
+   <Router>
+   <Routes>
+    <Route path="/Home"element={<Home/>} />
+    <Route path="/Profile"element={<Profile/>} />  
+    <Route path="/Sign-in"element={<SignIn/>} />
+    <Route path="/Sign-up"element={<SignUp/>} />
+    <Route path="/forgot-password"element={<ForgotPassword/>} />
+    <Route path="/offers"element={<Offers/>} />
+   
+   </Routes>
+ </Router>
+   
   );
 }
 
